0e76142906ed638029ac3a231900975232e7ada7
Antônio Campos <aclcampos@live.com>
2021-10-17 13:52:55 -0300

    Realizado requisito 6

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends React.Component {
     super();
 
     this.onInputChange = this.onInputChange.bind(this);
+    this.onSaveButtonClick = this.onSaveButtonClick.bind(this);
 
     this.state = {
       cardName: '',
@@ -19,6 +20,7 @@ class App extends React.Component {
       cardTrunfo: false,
       hasTrunfo: false,
       isSaveButtonDisabled: true,
+      savedCards: [],
     };
   }
 
@@ -28,6 +30,31 @@ class App extends React.Component {
     this.setState({
       [name]: value,
     }, () => this.buttonChange());
+  }
+
+  onSaveButtonClick() {
+    const { cardName, cardDescription,
+      cardAttr1, cardAttr2, cardAttr3,
+      cardImage, cardRare, cardTrunfo, savedCards } = this.state;
+    const addCard = {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+    };
+    savedCards.push(addCard);
+    this.setState({
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: '0',
+      cardAttr2: '0',
+      cardAttr3: '0',
+      cardImage: '',
+      cardRare: 'normal',
+    });
   }
 
   buttonChange() {
@@ -59,7 +86,11 @@ class App extends React.Component {
       <div>
         <header>Tryunfo</header>
         <section className="app">
-          <Form onInputChange={ this.onInputChange } { ...this.state } />
+          <Form
+            onSaveButtonClick={ this.onSaveButtonClick }
+            onInputChange={ this.onInputChange }
+            { ...this.state }
+          />
           <Card onInputChange={ this.onInputChange } { ...this.state } />
         </section>
       </div>
